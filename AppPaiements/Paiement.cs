631c0b55b3b33e384a88f32fa8f550ce5aa9721a
cheko victorine leonnelle <victorineleonnelle.cheko@monboreal.ca>
2024-10-25 10:05:38 -0700

    bonjour creation de Paiement

---
+++ AppPaiements/Paiement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Paiement
+    {
+        public string Description{ get; set; }
+        public double Montant { get; set; }
+        
+
+        public Paiement(string description,  double montant)
+        {
+            Description = description;
+            Montant = montant;
+            
+
+        }
+        public virtual void AfficherDetails()
+        {
+            Console.WriteLine($"les informations du paiement  sont:{Description}, {Montant} ");
+
+        }
+    }
+}
